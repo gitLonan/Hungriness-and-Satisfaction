6cae89e2f06830348fd11eef3bb51425696bf287
MujoHarac <sc2lazar@gmail.com>
2023-05-23 13:56:49 +0200

    tested nested tuples

--- Igrica_probanje.py
+++ Igrica_probanje.py
@@ -12,17 +12,38 @@
 #
 
 
-tags = {'broad': {'meat': (1,2), 'vegetable': (2, 1), 'sweet': (3,1), 'sandwich':(4,1)},
-        'specific': {'eggs': (4,1), 'bun': (5,1)}}
+# tags = {'broad': {'meat': (1,2), 'vegetable': (2, 1), 'sweet': (3,1), 'sandwich':(4,1)},
+#         'specific': {'eggs': (4,1), 'bun': (5,1)}}
 
-#print(tags['broad'])
-for i in tags:
-    print(tags[i])
-    for j in tags[i]:
-        print(j,tags[i][j][0])
+# #print(tags['broad'])
+# for i in tags:
+#     print(tags[i])
+#     for j in tags[i]:
+#         print(j,tags[i][j][0])
 
-call = 'asd'
+# call = 'asd'
 
-brick = ['12', '23', '23', call, '23']
-index = brick.index(call)
-print(index)
+# brick = ['12', '23', '23', call, '23']
+# index = brick.index(call)
+# print(index)
+
+
+distance_between_places_using_main_roud = (
+                            (('start_1','roud'), 100),
+                            (('start_2','roud'), 50),
+                            (('Zemun','roud'), 200),
+                            (('Vracar','roud'), 150),
+                            (('Novi Beograd', 'roud'), 300),
+                            (('Zvezdara','roud'), 350),
+                            (('Vozdovac','roud'), 450),
+                            (('Grocka','roud'), 200),
+                            )
+
+distance_between_places_that_are_connected = (
+                            (('Vracar','Zvezdara'), 200),
+                            (('Zemun','Novi Beograd'), 50),
+
+                                            )
+
+for i in distance_between_places_that_are_connected:
+    print(i[0])
